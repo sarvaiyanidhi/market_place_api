f5f0cf77eae701e3f07d156558c584534f511afd
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-11-09 22:53:35 -0500

    Post API for User creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # API Definition
   namespace :api, default: {format: :json} do
     namespace :v1 do
-      resources :users, only: [:show]
+      resources :users, only: %i[create show]
     end
   end
 end
