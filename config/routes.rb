0939dd24d278d2c1b59683bd481ca4b716fed1db
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-11-11 22:33:12 -0500

    Token creation for User

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'tokens/create'
+    end
+  end
   # API Definition
   namespace :api, default: {format: :json} do
     namespace :v1 do
       resources :users, only: %i[create update show destroy]
+      resources :tokens, only: [:create]
     end
   end
 end
