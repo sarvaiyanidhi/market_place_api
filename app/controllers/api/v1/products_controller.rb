e80582e5bafd0c01735e04e2bfa4cde7bde45ad2
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-11-15 13:27:10 -0500

    Product API for list and create

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,19 @@
 class Api::V1::ProductsController < ApplicationController
-  before_action :set_product
+  before_action :set_product, only: %i[show]
+  before_action :check_login, only: %i[create]
+
+  def index
+    render json: Product.all
+  end
+
+  def create
+    product = current_user.products.build(product_params)
+    if product.save
+      render json: product, status: :created
+    else
+      render json: { erros: product.errors}, status: :unprocessable_entity
+    end
+  end
 
   def show
     render json: @product
@@ -8,5 +22,9 @@ class Api::V1::ProductsController < ApplicationController
   private
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:title, :price, :published)
   end
 end
--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -6,10 +6,25 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     @product = products(:one)
   end
 
-  test 'Should show product' do
+  test 'should create product' do
+    assert_difference('Product.count') do
+      post api_v1_products_url,
+      params: {title: @product.title, price: @product.price, published: @product.published},
+      headers: {Authorization: JsonWebToken.encode(user_id: @product.user.id)},
+      as: :json
+    end
+    assert_response :created
+  end
+
+  test 'should show product' do
     get api_v1_product_url(@product), as: :json
     assert_response :success
     json_response = JSON.parse(self.response.body)
     assert_equal @product.title, json_response['title']
+  end
+
+  test 'should show products' do
+    get api_v1_products_url, as: :json
+    assert_response :success
   end
 end
