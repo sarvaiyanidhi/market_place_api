7e2099ad019eb088172038cb52bfa77fd54dcdd3
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-11-23 22:41:08 -0500

    Test for filter products by title

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0}, presence: true
   belongs_to :user
 
-  scope: filter_by_title, lambda { |keyword| where('lower(title) like ?', "%#{keyword.downcase}%")}
+  scope :filter_by_title, lambda { |keyword| where('lower(title) like ?', "%#{keyword.downcase}%")}
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -6,4 +6,12 @@ class ProductTest < ActiveSupport::TestCase
     product.price = -1
     assert_not product.valid?
   end
+
+  test "should filter products by title" do
+    assert_equal 2, Product.filter_by_title("tv").count
+  end
+
+  test "Should filter products by tittle and sort it" do
+    assert_equal [products(:another_tv), products(:one)], Product.filter_by_title("tv").sort
+  end
 end
