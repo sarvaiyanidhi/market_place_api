9c9d6d35de20d2456450099e0ebe5034ca8e52a8
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-11-23 22:58:50 -0500

    Test for price filter in products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -14,4 +14,12 @@ class ProductTest < ActiveSupport::TestCase
   test "Should filter products by tittle and sort it" do
     assert_equal [products(:another_tv), products(:one)], Product.filter_by_title("tv").sort
   end
+
+  test "should filter products by higher price and sort them" do
+    assert_equal [products(:two), products(:one)], Product.above_or_equal_to_price(200).sort
+  end
+
+  test "should filter products by lower price and sort them" do
+    assert_equal [products(:another_tv)], Product.below_or_equal_to_price(200).sort
+  end
 end
