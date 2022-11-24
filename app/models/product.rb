c2f9913583a366d78a0f30f61060d891d68347a1
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-11-23 22:52:02 -0500

    Scope to filter products by price

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,6 @@ class Product < ApplicationRecord
   belongs_to :user
 
   scope :filter_by_title, lambda { |keyword| where('lower(title) like ?', "%#{keyword.downcase}%")}
+  scope :above_or_equal_to_price, lambda {|price| where('price >= ?', price)}
+  scope :below_or_equal_to_price, lambda {|price| where('price <= ?', price)}
 end
